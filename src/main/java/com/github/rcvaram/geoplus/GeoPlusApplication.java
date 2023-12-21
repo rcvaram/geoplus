a4619fb6995b967f2e4bdc352597e33ad0cec3e7
NextGen Dev <pranavan@nextgeninnovations.co>
2023-12-20 19:02:07 -0800

    init commit

---
+++ src/main/java/com/github/rcvaram/geoplus/GeoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.github.rcvaram.geoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoPlusApplication.class, args);
+    }
+
+}
