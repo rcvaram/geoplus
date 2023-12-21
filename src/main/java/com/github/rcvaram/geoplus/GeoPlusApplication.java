72b74f2032eb96c604f5634243a4599c833277c6
sivaram <pranavan@nextgeninnovations.co>
2023-12-20 19:03:58 -0800

    fixed username

--- src/main/java/com/github/rcvaram/geoplus/GeoPlusApplication.java
+++ src/main/java/com/github/rcvaram/geoplus/GeoPlusApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeoPlusApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GeoPlusApplication.class, args);
     }
